91399852e8648ec4672612ce7471d909049fb3e9
UraraEiss-Work <cygeiss@163.com>
2021-07-15 17:59:23 +0800

    字符串

--- venv/string.py
+++ venv/string.py
@@ -5,7 +5,30 @@
 # print(a [2:10])
 
 # Escape character
-list =  ['cao','you','gai']
-with open('testfile','w') as f:
+'''
+list =  ['曹','犹','盖']
+with open('testfile','w', encoding ='utf-8') as f:
     for name in list :
         f.write(name + '\n')
+'''
+#format
+# string = "I like {color}".format(color='red')
+# print(string)
+
+# info = {'vehicle':'plane','place':'new york'}
+# print(f"我要搭乘info['vehicle']去往info['place']")
+'''
+索引的插入与追加
+list = [1,2,3,4,5]
+list.insert(3,8)
+print(list)
+
+lst = ['a','b','c']
+lst.insert(0,'r')
+print(lst)
+
+ls = ['a','b','c']
+ls.append('d')
+print(ls)
+'''
+
