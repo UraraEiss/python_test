2ef6a2af1f5d853f887196b8d4a56d292eae1bb6
UraraEiss-Work <cygeiss@163.com>
2021-07-15 09:55:22 +0800

    二、八、十、十六进制的转换

---
+++ venv/jingzhizhuanghuan.py
@@ -0,0 +1,22 @@
+# /usr/bin/env python
+# _*_coding:UTF-8 _*_
+'''二、八、十六进十
+value = int('0b01001101' ,2)
+print(value)
+value2 = int('0o16271' ,8)
+print(value2)
+value3 = int('0x2d4e', 16)
+print(value3)
+'''
+
+'''十进二、八、十六
+print(bin(114514))
+print(oct(114514))
+print(hex(114514))
+'''
+'''二八互转
+print(bin(int('337522' , 8))) #需要先将八进制转换成十进制，再转化为二进制
+print(oct(0b1010101))   
+'''
+
+# print(bin(int('1d7e8c' , 16)))
