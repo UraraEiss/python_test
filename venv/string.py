e9b5d294353565867914ac09d6571625929619ae
UraraEiss-Work <cygeiss@163.com>
2021-07-20 14:41:35 +0800

    字符串

--- venv/string.py
+++ venv/string.py
@@ -32,3 +32,62 @@ ls.append('d')
 print(ls)
 '''
 
+'''
+list = [0,1,2,3,2,1,3,4,1,4,5,0,0]
+print(list.count(0))
+print(list.count(4))
+print(list.count(1))
+'''
+
+''''
+list1 = [1,1,1]
+list2 = [2,2,2]
+print(id(list1))
+print(id(list2))
+list1.extend(list2)
+print(list1)
+print(id(list1))
+list2.append(3)
+print(list2)
+print(id(list2))
+'''
+
+'''
+study_dict = {
+    'monday_dict' : {
+      '校区': "东区",
+      '楼号': 9,
+      '教室': 9104
+     },
+
+    'tuesday_dict' : {
+      '校区':'西区',
+      '楼号': 2,
+      '教室': 2204
+     }
+}
+print(study_dict['tuesday_dict'])
+'''
+'''
+stu_dict = {'数学',92,
+            '语文',93
+           }
+stu2_dict = {'英语',78}
+stu_dict.update(stu2_dict)
+'''
+'''
+dict = {
+    'python':6,
+    'java':4,
+    'c':1
+}
+dict ['php'] =3
+dict ['javascript'] = 7
+print(max(dict.values()))
+'''
+
+set1 = set()
+set2 = {'python','java','php','c'}
+
+set2.add('c++')
+print('java' in set2)
