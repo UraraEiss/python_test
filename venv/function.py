5699ca2abd5426c7da1866528267705ecdf28611
UraraEiss-Work <cygeiss@163.com>
2021-07-26 16:47:57 +0800

    函数-输出菱形-钻石

--- venv/function.py
+++ venv/function.py
@@ -2,7 +2,7 @@
 # _*_coding:UTF-8 _*_
 # def hello_world():
 #     print('hello world')
-
+'''
 def diamond(number):
     for i in range(number):
         if i <= (number//2):
@@ -10,3 +10,14 @@ def diamond(number):
         else:
             print(" "*(i-number//2) + "*"*((number-i)*2-1))
 diamond(11)
+'''
+'''
+def diamond(number):
+    for i in range(number):
+        if (number//2-1) <= i <= (number//2)  :
+            print(" "*(number//2-i) + "*"*(2*i+1))
+
+        if i> (number//2):
+            print(" "*(i-number//2) + "*"*((number-i)*2-1))
+diamond(11)
+'''
